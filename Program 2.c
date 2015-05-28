0535d05e95f4d5a1982404db30247adf95fdc700
kubaqwerty1234 <kubaqwerty1234@gmail.com>
2015-05-28 14:16:18 +0200

    2

--- Program 2.c
+++ Program 2.c
@@ -1,28 +1,28 @@
 int playchessgame(){
-         
+
         char wmove[8]={"0"};
         char bmove[8]={"0"};
         int gameinplay =0;
         char movehistory[650]={"0"};
         char gamehistory[950]={"0"};
         char mate[]={"mate"};
-         
+
         while(gameinplay==0){
-                     
+
         printf("\nWhite to play.Enter move:\n");
         scanf("%s",wmove);
         if(strcmp(wmove,mate)== 0){
             printf("Game over CHECKMATE\n");
             break;
         }
-                 
+
         printf("Black to play.Enter move:\n");
         scanf("%s",bmove);
         if(strcmp(bmove,mate)== 0){
             printf("Game over CHECKMATE\n");
             break;
         }
-                 
+
         strcat(wmove,bmove);
         strcpy(movehistory,wmove);
         strcat(gamehistory,movehistory);
